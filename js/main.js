eab26e2131251e91fee6cddb7e85b67339e10046
Jared Gutkin <jgutkinsam@gmail.com>
2022-04-30 18:07:33 -0500

    set up fetch, started to assign variable

--- js/main.js
+++ js/main.js
@@ -7,11 +7,22 @@ const btn = document.getElementById("btn")
 let getPokeData = () => {
     // Generate random number to get pokemon
     let id = Math.floor(Math.random() * 150) + 1 
-    console.log(id)
-
+    //combine url and pokemon id
     const finalUrl = url + id 
-    console.log(finalUrl)
+    //fetch
+    fetch(finalUrl).then((response) => response.json())
+    .then((data) => {
+        generateCard(data)
+    })
+}
+
+//generate card
+let generateCard = (data) => {
+    //get necessary data and assign to variables
+    const hp = data.stats[0].base_stat
+    console.log(hp)
 }
 
 btn.addEventListener("click", getPokeData)
+window.addEventListener("load", getPokeData)
 
