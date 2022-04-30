4bf71f372189f9d69e2e229f68074b32bfe2a9ed
Jared Gutkin <jgutkinsam@gmail.com>
2022-04-30 18:13:34 -0500

    variables assigned

--- js/main.js
+++ js/main.js
@@ -20,7 +20,13 @@ let getPokeData = () => {
 let generateCard = (data) => {
     //get necessary data and assign to variables
     const hp = data.stats[0].base_stat
-    console.log(hp)
+    const imgSrc = data.sprites.other.dream_world.front_default
+    const pokeName = data.name 
+    const statAttack = data.stats[1].base_stat
+    const statDefense = data.stats[2].base_stat
+    const statSpeed = data.stats[5].base_stat
+
+    card.innerHTML = ""
 }
 
 btn.addEventListener("click", getPokeData)
