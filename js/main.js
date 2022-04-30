48546cae4f34e62e85e1431236cd84aed238ae26
Jared Gutkin <jgutkinsam@gmail.com>
2022-04-30 17:57:24 -0500

    js started

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+const url = " https://pokeapi.co/api/v2/pokemon/"
+
+const card = document.getElementById("card")
+const btn = document.getElementById("btn")
+
+
+let getPokeData = () => {
+    // Generate random number to get pokemon
+    let id = Math.floor(Math.random() * 150) + 1 
+    console.log(id)
+
+    const finalUrl = url + id 
+    console.log(finalUrl)
+}
+
+btn.addEventListener("click", getPokeData)
 
