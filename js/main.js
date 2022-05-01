46d62fab4102a10c2a9602710c86778dc7c268b8
Jared Gutkin <jgutkinsam@gmail.com>
2022-04-30 20:16:23 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 const typeColor = {
     bug: "#26de81",
-    dragon: "ffeaa7",
-    electric: "fed330",
-    fairy: "FF0069",
-    fighting: "30336b",
-    fire: "f0932b",
-    flying: "81ecec",
-    grass: "00b894",
-    ground: "EFB549",
-    ghost: "a55eea",
-    ice: "74b9ff",
-    normal: "95afc0",
-    poison: "6c5ce7",
-    psychic: "a29bfe",
+    dragon: "#ffeaa7",
+    electric: "#fed330",
+    fairy: "#FF0069",
+    fighting: "#30336b",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#00b894",
+    ground: "#EFB549",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
     rock: "#2d3436",
     water: "#0190FF",
 }
@@ -38,10 +38,13 @@ let generateCard = (data) => {
     //get necessary data and assign to variables
     const hp = data.stats[0].base_stat
     const imgSrc = data.sprites.other.dream_world.front_default
-    const pokeName = data.name 
+    const pokeName = data.name[0].toUpperCase() + data.name.slice(1) 
     const statAttack = data.stats[1].base_stat
     const statDefense = data.stats[2].base_stat
     const statSpeed = data.stats[5].base_stat
+
+    //set theme color based on type
+    const themeColor = typeColor[data.types[0].type.name]
 
     card.innerHTML = `
         <p class="hp">
@@ -71,7 +74,8 @@ let generateCard = (data) => {
     
     
     `
-    appendTypes(data.types)
+    appendTypes(data.types);
+    styleCard(themeColor)
 }
 
 let appendTypes = (types) => {
@@ -79,6 +83,14 @@ let appendTypes = (types) => {
         let span = document.createElement("SPAN")
         span.textContent = item.type.name
         document.querySelector(".types").appendChild(span)
+    })
+}
+
+let styleCard = (color) => {
+    card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
+
+    card.querySelectorAll(".types span").forEach(typeColor => {
+        typeColor.style.backgroundColor = color
     })
 }
 
